b2e3118fd55a2a204fbdb6677ccfd5d3c6bb869c
Anatoly Bubenkov <bubenkoff@gmail.com>
2013-05-28 12:51:12 +0200

    version up

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     description='Splinter subplugin for Pytest BDD plugin',
     author='Oleg Pidsadnyi',
     author_email='oleg.podsadny@gmail.com',
-    version='0.2',
+    version='0.3',
     cmdclass={'test': PyTest},
     install_requires=[
         'setuptools',
