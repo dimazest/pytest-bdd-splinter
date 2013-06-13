12554a2e8edec5c25346ea49d8dc325c7a2beef6
Dirk Zittersteyn <dirk@dirk.(none)>
2013-06-13 15:32:31 +0200

    fixed an issue with mouse_over in firefox

---
+++ pytest_bdd_splinter/splinter_patches.py
@@ -0,0 +1,18 @@
+"""Patches for splinter"""
+
+from selenium.webdriver.common.action_chains import ActionChains
+from splinter.driver.webdriver.firefox import WebDriverElement
+
+def patch_webdriverelement():
+    """Patches the WebDriverElement to allow firefox to use mouse_over"""
+    def mouse_over(self):
+        """
+        Performs a mouse over the element.
+
+        Currently works only on Chrome driver.
+        """
+        ActionChains(self.parent.driver).move_to_element(self._element).perform()
+
+
+    # Apply the monkey patch for Firefox WebDriverElement
+    WebDriverElement.mouse_over = mouse_over
